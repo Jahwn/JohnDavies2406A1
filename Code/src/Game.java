7b1a19d4d857408379a5d4122687dec941b72c32
John Davies <john.davies@my.jcu.edu.au>
2016-09-19 12:09:26 +1000

    Player turn mechanic fully working
    Players are now able to draw and compare cards. Super trump cards also work. There is still no winning condition.

--- Code/src/Game.java
+++ Code/src/Game.java
@@ -24,6 +24,7 @@ public class Game {
 
         // Will contain current category
         String category = "No category";
+        // Will contain value of card presently on the table (value in current category)
         Double categoryValue = 0.0;
 
         JOptionPane.showMessageDialog(null, "Welcome to the Mineral Super Trumps Game!");
@@ -42,111 +43,140 @@ public class Game {
         }
         // Assigning cards to players
         for (Player s: players) {
-            System.out.println("Testing Player " + s.playerNo);
             while(s.pCards.size() <= 7) {
                 s.pCards.add(deck.get(0));
                 deck.remove(0);
             }
         }
 
+        Boolean running = true;
 
         // This is where players take their turns
-        for (Player p: players) {
-            System.out.println("Player " + p.playerNo + "'s turn");
-            Card playerChoice = p.playerTurn();
+        while (running) {
+            for (Player p : players) {
+                System.out.println("Player " + p.playerNo + "'s turn");
+                Card playerChoice = p.playerTurn();
+                boolean cardChosen = false;
 
-            // If the player has chosen to pass
-            if (playerChoice == null) {
-                p.pCards.add(deck.get(0));
-                System.out.println(deck.get(0).getName() + " added to your hand Player " + p.playerNo);
-                deck.remove(0);
-            } else {
-                Boolean isTrumpCard = cardData.isTrumpCard(playerChoice.name);
-                if (isTrumpCard) { // If a trump card is detected, the category is automatically changed
-                    if (playerChoice.name.equals("The Geologist")) {
-                        System.out.println("Select a category: " +
-                                "\n0 Hardness" +
-                                "\n1 Specific Gravity" +
-                                "\n2 Cleavage" +
-                                "\n3 Crustal Abundance" +
-                                "\n4 Economic Value");
-                        int input = reader.nextInt();
-                        if (input == 0) {
-                            category = "Hardness";
-                        } else if (input == 1) {
-                            category = "Specific Gravity";
-                        } else if (input == 2) {
-                            category = "Cleavage";
-                        } else if (input == 3) {
-                            category = "Crustal Abundance";
-                        } else if (input == 4) {
-                            category = "Economic Value";
-                        }
-                    } else {
-                        System.out.println(playerChoice.getName() + "'s description: " +
-                                "\n" + playerChoice.getCategory());
-                        category = playerChoice.getCategory();
-                    }
-                    System.out.println("----------------------------------------" +
-                            "\nNew category is " + category +
-                            "\n----------------------------------------");
+                // If the player has chosen to pass
+                if (playerChoice == null) {
+                    p.pCards.add(deck.get(0));
+                    System.out.println(deck.get(0).getName() + " added to your hand Player " + p.playerNo);
+                    deck.remove(0);
                 } else {
-                    System.out.println(playerChoice.getName() + "'s data:");
-                    System.out.println("0 Hardness: " + playerChoice.getHardness() +
-                            "\n1 Specific Gravity: " + playerChoice.getSpecificGravity() +
-                            "\n2 " + playerChoice.getCleavage() +
-                            "\n3 Crustal Abundance: " + playerChoice.getCrustalAbundance() +
-                            "\n4 Economic Value: " + playerChoice.getEconomicValue() +
-                            "\n4 " + playerChoice.getCategory());
-
-                    // If a category already exists then players don't choose a category
-                    if (category.equals("No category")) {
-                        table = playerChoice;
-                        System.out.print("Enter the category by inputting the number next to the category name: ");
-                        int input = reader.nextInt();
-                        if (input == 0) {
-                            category = "Hardness";
-                            categoryValue = playerChoice.getHardness();
-                        } else if (input == 1) {
-                            category = "Specific Gravity";
-                            categoryValue = playerChoice.getSpecificGravity();
-                        } else if (input == 2) {
-                            category = "Cleavage";
-                            categoryValue = null;
-                        } else if (input == 3) {
-                            category = "Crustal Abundance";
-                            categoryValue = playerChoice.getCrustalAbundance();
-                        } else if (input == 4) {
-                            category = "Economic Value";
-                            categoryValue = playerChoice.getEconomicValue();
-                        }
-                        System.out.println("Category: " + category + " | value: " + categoryValue);
-                        System.out.println();
-                    } else {
-                        Double playerCategoryValue = 0.0;
-
-                        if (category.equals("Hardness")) {
-                            playerCategoryValue = playerChoice.getHardness();
-                        } else if (category.equals("Specific Gravity")) {
-                            playerCategoryValue = playerChoice.getSpecificGravity();
-                        } else if (category.equals("Cleavage")) {
-                            category = null;
-                        } else if (category.equals("Crustal Abundance")) {
-                            playerCategoryValue = playerChoice.getCrustalAbundance();
-                        } else if (category.equals("Economic Value")) {
-                            playerCategoryValue = playerChoice.getEconomicValue();
-                        }
-
-                        if (categoryValue > playerCategoryValue) {
-                            System.out.println("Your cards value in " + category + " is lower than " + table.name);
+                    boolean choiceValid = false;
+                    while (!choiceValid) {
+                        System.out.println(playerChoice.getName() + "'s data:");
+                        System.out.println("0 Hardness: " + playerChoice.getHardness() +
+                                "\n1 Specific Gravity: " + playerChoice.getSpecificGravity() +
+                                "\n2 " + playerChoice.getCleavage() +
+                                "\n3 Crustal Abundance: " + playerChoice.getCrustalAbundance() +
+                                "\n4 Economic Value: " + playerChoice.getEconomicValue() +
+                                "\n4 " + playerChoice.getCategory());
+                        System.out.print("Do you want to proceed with this card? Press 'y', or 'n' to proceed: ");
+                        String choice = reader.next();
+                        if (choice.equals("y")) {
+                            choiceValid = true;
+                        } else if (choice.equals("n")) {
+                            System.out.println("Select another card or pass.");
+                            p.pCards.add(playerChoice);
+                            playerChoice = p.playerTurn();
                         } else {
-                            System.out.println("Your card beat " + table.name + "!");
-                            table = playerChoice;
-                            categoryValue = playerCategoryValue;
-                            System.out.println("New value to beat for " + category + " is: " + categoryValue +
-                                    "\n----------------------------------------");
-                            System.out.println();
+                            System.out.print("Please enter an proper input: ");
                         }
+                    }
+                    while (!cardChosen) {
+                        System.out.println(playerChoice.getName() + "'s data:");
+                        System.out.println("0 Hardness: " + playerChoice.getHardness() +
+                                "\n1 Specific Gravity: " + playerChoice.getSpecificGravity() +
+                                "\n2 " + playerChoice.getCleavage() +
+                                "\n3 Crustal Abundance: " + playerChoice.getCrustalAbundance() +
+                                "\n4 Economic Value: " + playerChoice.getEconomicValue() +
+                                "\n4 " + playerChoice.getCategory());
+                        Boolean isTrumpCard = cardData.isTrumpCard(playerChoice.name);
+                        if (isTrumpCard) { // If a trump card is detected, the category is automatically changed
+                            if (playerChoice.name.equals("The Geologist")) {
+                                System.out.println("Select a category: " +
+                                        "\n0 Hardness" +
+                                        "\n1 Specific Gravity" +
+                                        "\n2 Cleavage" +
+                                        "\n3 Crustal Abundance" +
+                                        "\n4 Economic Value");
+                                int input = reader.nextInt();
+                                if (input == 0) {
+                                    category = "Hardness";
+                                } else if (input == 1) {
+                                    category = "Specific Gravity";
+                                } else if (input == 2) {
+                                    category = "Cleavage";
+                                } else if (input == 3) {
+                                    category = "Crustal Abundance";
+                                } else if (input == 4) {
+                                    category = "Economic Value";
+                                }
+                                categoryValue = 0.0;
+                                System.out.println("Category value has been reset");
+                            } else {
+                                System.out.println(playerChoice.getName() + "'s description: " +
+                                        "\n" + playerChoice.getCategory());
+                                category = playerChoice.getCategory();
+                                categoryValue = 0.0;
+                                System.out.println("Category value has been reset");
+                            }
+                            System.out.println("----------------------------------------" +
+                                    "\nNew category is " + category +
+                                    "\n----------------------------------------");
+                        } else {
+                            // If a category already exists then players don't choose a category
+                            if (category.equals("No category")) {
+                                table = playerChoice;
+                                System.out.print("Enter the category by inputting the number next to the category name: ");
+                                int input = reader.nextInt();
+                                if (input == 0) {
+                                    category = "Hardness";
+                                    categoryValue = playerChoice.getHardness();
+                                } else if (input == 1) {
+                                    category = "Specific Gravity";
+                                    categoryValue = playerChoice.getSpecificGravity();
+                                } else if (input == 2) {
+                                    category = "Cleavage";
+                                    categoryValue = null;
+                                } else if (input == 3) {
+                                    category = "Crustal Abundance";
+                                    categoryValue = playerChoice.getCrustalAbundance();
+                                } else if (input == 4) {
+                                    category = "Economic Value";
+                                    categoryValue = playerChoice.getEconomicValue();
+                                }
+                                System.out.println("Category: " + category + " | value: " + categoryValue);
+                                System.out.println();
+                            } else {
+                                Double playerCategoryValue = 0.0;
+
+                                if (category.equals("Hardness")) {
+                                    playerCategoryValue = playerChoice.getHardness();
+                                } else if (category.equals("Specific Gravity")) {
+                                    playerCategoryValue = playerChoice.getSpecificGravity();
+                                } else if (category.equals("Cleavage")) {
+                                    category = null;
+                                } else if (category.equals("Crustal Abundance")) {
+                                    playerCategoryValue = playerChoice.getCrustalAbundance();
+                                } else if (category.equals("Economic Value")) {
+                                    playerCategoryValue = playerChoice.getEconomicValue();
+                                }
+
+                                if (categoryValue > playerCategoryValue) {
+                                    System.out.println("Your cards value in " + category + " is lower than " + table.name);
+                                } else {
+                                    System.out.println("Your card beat " + table.name + "!");
+                                    table = playerChoice;
+                                    categoryValue = playerCategoryValue;
+                                    System.out.println("New value to beat for " + category + " is: " + categoryValue);
+                                    System.out.println();
+                                }
+                            }
+                        }
+                        cardChosen = true;
                     }
                 }
             }
